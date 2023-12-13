16512243418659f96b27e1af66df530c12e2ef7c
mhodge11 <micahhodgeutk@icloud.com>
2023-12-13 13:36:46 -0500

    Adding describe script and READMEs

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Welcome to Advent of TypeScript!");
+import chalk from "chalk";
+
+console.log("");
+console.log(chalk.bold.bgBlue("Welcome to Advent of TypeScript!"));
+console.log("");
+console.log(
+  `Use ${chalk.bold.blueBright("bun run day-<day>")} to run a day's solution.`
+);
